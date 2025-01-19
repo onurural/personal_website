bcf8bc271c04523561a3a616f610fca084dccd5a
onurural <54706493+onurural@users.noreply.github.com>
2025-01-19 23:51:46 +0000

    added cname file

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
